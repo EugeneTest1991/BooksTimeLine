c17cf8a0f721660ae8d3ba65b2b333300a04070b
EugeneTest1991 <haiduchonak@gmail.com>
2014-10-10 17:24:04 +0300

    create project and Book class

---
+++ src/com/haid/timeline/domain/Book.java
@@ -0,0 +1,55 @@
+package com.haid.timeline.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public final class Book implements Serializable {
+
+    private static final long serialVersionUID = 0L;
+    private Long id;
+    private String author;
+    private String title;
+    private Date actionStart;
+    private Date actionEnd;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getActionStart() {
+        return actionStart;
+    }
+
+    public void setActionStart(Date actionStart) {
+        this.actionStart = actionStart;
+    }
+
+    public Date getActionEnd() {
+        return actionEnd;
+    }
+
+    public void setActionEnd(Date actionEnd) {
+        this.actionEnd = actionEnd;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
