21d9c073b621121ca1a2c389b79ba3cb7af87979
Eugene G <haiduchonak@gmail.com>
2014-10-11 23:01:01 +0300

    create dao stub

---
+++ src/com/haid/timeline/repository/StubBooksDAO.java
@@ -0,0 +1,28 @@
+package com.haid.timeline.repository;
+
+import java.util.Date;
+import java.util.List;
+
+import com.haid.timeline.domain.Book;
+
+public class StubBooksDAO implements BooksDAO {
+
+    @Override
+    public List<Book> getAllBooks() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Book> getBooksFromPeriod(Date dateFrom, Date dateto) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Book getBookById(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
