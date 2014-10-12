523d390b6ba945149ec71311f402a7c49ab8ee0f
Eugene G <haiduchonak@gmail.com>
2014-10-12 23:23:58 +0300

    create stub tests for dao

---
+++ test/com/haid/timeline/repository/StubBooksDAOTests.java
@@ -0,0 +1,28 @@
+package com.haid.timeline.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StubBooksDAOTests {
+
+    @Before
+    public void setUp() throws Exception {}
+
+    @Test
+    public void testGetAllBooks() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetBooksFromPeriod() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetBookById() {
+        fail("Not yet implemented");
+    }
+
+}
