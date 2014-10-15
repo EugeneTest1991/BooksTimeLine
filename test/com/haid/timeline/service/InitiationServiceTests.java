1aaa8849e2827dda4c6ddbe6cb549a2d03e2dc37
EugeneTest1991 <haiduchonak@gmail.com>
2014-10-15 11:28:16 +0300

    create InitiationService Tests

---
+++ test/com/haid/timeline/service/InitiationServiceTests.java
@@ -0,0 +1,21 @@
+package com.haid.timeline.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class InitiationServiceTests {
+
+    @Before
+    public void setUp() throws Exception {}
+
+    @Test
+    public final void testInitBookManager() {
+        BookManager bookManager = InitiationService.initBookManager();
+        assertNotNull("not init BookManager", bookManager);
+        assertNotNull("not init books collection", bookManager.getAllBooks());
+        assertNotEquals("empty books collection", 0L, bookManager.getAllBooks().size());
+    }
+
+}
